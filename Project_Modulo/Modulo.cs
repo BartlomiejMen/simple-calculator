55dd6d2877c5fb3015bda4627a2386ed62da290d
BartlomiejMen <bartlomiej.men@hotmail.com>
2020-12-06 22:42:38 +0100

    Project Modulo v1

---
+++ Project_Modulo/Modulo.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_Modulo
+{
+    class Modulo
+    {
+        public static string ModuloMethod()
+        {
+            int variable = (GetNumber() % 2);
+            if (variable == 0)
+            {
+                return "Podana liczba jest liczbą parzystą";
+            }
+            else
+            {
+                return "Podana liczba jest liczbą nieparzystą";
+            }
+
+        }
+
+        private static int GetNumber()
+        {
+            if (!int.TryParse(Console.ReadLine(), out int input))
+
+                throw new Exception("Podana wartośc jeat nieprawidłowa.");
+
+            return input;
+        }
+    }
+
+}
---
+++ Project_Modulo/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_Modulo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Podaj liczbę do sprawdzenia: ");
+            Console.WriteLine(Modulo.ModuloMethod());
+        }
+    }
+}
