a01421f40bcdfd3704aa6f87245f203966c3f1ad
BartlomiejMen <bartlomiej.men@hotmail.com>
2020-12-06 20:22:10 +0100

    Add project files

---
+++ LEKCJA_21_Praca Domowa/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LEKCJA_21_Praca_Domowa
+{
+    class Program
+    {
+        private static DateTime actualDate;
+
+        static void Main(string[] args)
+        {
+            string name;
+            string placeOfBirth;
+            
+            var actualDate = DateTime.Now;
+            var toDay = actualDate.Day;
+            var year = actualDate.Year;
+
+            Console.WriteLine(actualDate);
+            Console.WriteLine(toDay);
+            Console.WriteLine(year);
+        }
+    }
+}
