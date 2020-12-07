6676fcbd2a998451664244d1a86d1b5f2c5be7b4
BartlomiejMen <bartlomiej.men@hotmail.com>
2020-12-07 20:25:14 +0100

    FizzBuzz v1

---
+++ Project_FizzBuzz/FizzBuzz.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_FizzBuzz
+{
+    public class FizzBuzz
+    {
+        private int _variable;
+
+        public FizzBuzz(string number)
+        {
+            _variable = GetNumber(number);
+            Console.WriteLine(FizzBuzzMethod(_variable));
+
+        }
+
+
+        private string FizzBuzzMethod(int variable)
+        {
+            //string result = null;
+            string result = FizzMethod(variable) + BuzzMethod(variable);
+
+            if (result != "") //domyślnie miało być: if( result != null)
+            {
+                return result;
+            }
+            else
+            {
+                return _variable.ToString();
+            }
+
+        }
+
+
+        private String FizzMethod(int variable)
+        {
+            int resultOfModulo3 = (variable % 3);
+
+            if (resultOfModulo3 == 0)
+            {
+                return "Fizz";
+            }
+            else
+            {
+                return null;
+            }
+
+        }
+
+        private string BuzzMethod(int variable)
+        {
+
+            int resultOfModulo5 = (variable % 5);
+            if (resultOfModulo5 == 0)
+            {
+                return "Buzz";
+            }
+            else
+            {
+                return null;
+            }
+
+        }
+
+        int GetNumber(string number)
+        {
+            if (!int.TryParse(number, out int input))
+
+                throw new Exception("Podana wartośc jeat nieprawidłowa.");
+
+            return input;
+        }
+
+    }
+}
---
+++ Project_FizzBuzz/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Write("Podaj liczbę: ");
+                FizzBuzz fizzBuzz = new FizzBuzz(Console.ReadLine());
+            }
+
+        }
+    }
+}
